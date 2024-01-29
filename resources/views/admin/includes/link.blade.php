87a918070751d24eb15b135ebb87c68a5055556b
faiz86Ahmad <faizan89ahmad@gmail.com>
2024-01-29 18:44:52 +0530

    dashboard blade

--- resources/views/admin/includes/link.blade.php
+++ resources/views/admin/includes/link.blade.php
@@ -14,13 +14,15 @@
     <link rel="stylesheet" href="{{ asset('assets/admin/css/bootstrap.min.css') }}">
     {{-- <link rel="stylesheet" href="{{ asset('assets/admin/fonts/nalika.ttf') }}"> --}}
     {{-- <link rel="stylesheet" href="{{ asset('assets/admin/fonts/nalika.woff') }}"> --}}
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 
     <!-- Bootstrap CSS
 		============================================ -->
     <link rel="stylesheet" href="{{ asset('assets/admin/css/font-awesome.min.css') }}">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css"/>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" />
 
-	<!-- nalika Icon CSS
+    <!-- nalika Icon CSS
 		============================================ -->
     <link rel="stylesheet" href="{{ asset('assets/admin/css/nalika-icon.css') }}">
     <!-- owl.carousel CSS
@@ -65,7 +67,7 @@
     <!-- Include DataTables CSS -->
     {{-- <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.11.5/css/jquery.dataTables.min.css"> --}}
     <link href="{{ asset('assets/admin/css/dataTables.min.css') }}" rel="stylesheet">
- 
+
     <!-- responsive CSS
 		============================================ -->
     <link rel="stylesheet" href="{{ asset('assets/admin/css/responsive.css') }}">
@@ -78,47 +80,52 @@
 <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 <!-- jquery
 		============================================ -->
-    <script src="{{ asset('assets/admin/js/vendor/jquery-1.12.4.min.js') }}"></script>
-    <!-- bootstrap JS
+<script src="{{ asset('assets/admin/js/vendor/jquery-1.12.4.min.js') }}"></script>
+<!-- bootstrap JS
     ============================================ -->
-    <script src="{{ asset('assets/admin/js/bootstrap.min.js') }}"></script>
-    <!-- wow JS
+<script src="{{ asset('assets/admin/js/bootstrap.min.js') }}"></script>
+<!-- wow JS
     ============================================ -->
-    <script src="{{ asset('assets/admin/js/wow.min.js') }}"></script>
-    <!-- price-slider JS
+<script src="{{ asset('assets/admin/js/wow.min.js') }}"></script>
+<!-- price-slider JS
     ============================================ -->
-    <script src="{{ asset('assets/admin/js/jquery-price-slider.js') }}"></script>
-    <!-- meanmenu JS
+<script src="{{ asset('assets/admin/js/jquery-price-slider.js') }}"></script>
+<!-- meanmenu JS
     ============================================ -->
-    <script src="{{ asset('assets/admin/js/jquery.meanmenu.js') }}"></script>
-    <!-- owl.carousel JS
+<script src="{{ asset('assets/admin/js/jquery.meanmenu.js') }}"></script>
+<!-- owl.carousel JS
     ============================================ -->
-    <script src="{{ asset('assets/admin/js/owl.carousel.min.js') }}"></script>
-    <!-- sticky JS
+<script src="{{ asset('assets/admin/js/owl.carousel.min.js') }}"></script>
+<!-- sticky JS
     ============================================ -->
-    <script src="{{ asset('assets/admin/js/jquery.sticky.js') }}"></script>
-    <!-- scrollUp JS
+<script src="{{ asset('assets/admin/js/jquery.sticky.js') }}"></script>
+<!-- scrollUp JS
     ============================================ -->
-    <script src="{{ asset('assets/admin/js/jquery.scrollUp.min.js') }}"></script>
-    <!-- mCustomScrollbar JS
+<script src="{{ asset('assets/admin/js/jquery.scrollUp.min.js') }}"></script>
+<!-- mCustomScrollbar JS
     ============================================ -->
-    <script src="{{ asset('assets/admin/js/scrollbar/jquery.mCustomScrollbar.concat.min.js') }}"></script>
-    <script src="{{ asset('assets/admin/js/scrollbar/mCustomScrollbar-active.js') }}"></script>
-    <!-- sparkline JS
+<script src="{{ asset('assets/admin/js/scrollbar/jquery.mCustomScrollbar.concat.min.js') }}"></script>
+<script src="{{ asset('assets/admin/js/scrollbar/mCustomScrollbar-active.js') }}"></script>
+<!-- sparkline JS
     ============================================ -->
-    <script src="{{ asset('assets/admin/js/sparkline/jquery.sparkline.min.js') }}"></script>
-    <script src="{{ asset('assets/admin/js/sparkline/jquery.charts-sparkline.js') }}"></script>
-    <!-- calendar JS
+<script src="{{ asset('assets/admin/js/sparkline/jquery.sparkline.min.js') }}"></script>
+<script src="{{ asset('assets/admin/js/sparkline/jquery.charts-sparkline.js') }}"></script>
+<!-- calendar JS
     ============================================ -->
-    <script src="{{ asset('assets/admin/js/calendar/moment.min.js') }}"></script>
-    <script src="{{ asset('assets/admin/js/calendar/fullcalendar.min.js') }}"></script>
-    <script src="{{ asset('assets/admin/js/calendar/fullcalendar-active.js') }}"></script>
-    <!-- Include DataTables JS -->
-    {{-- <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.11.5/js/jquery.dataTables.min.js"></script> --}}
-    
+<script src="{{ asset('assets/admin/js/calendar/moment.min.js') }}"></script>
+<script src="{{ asset('assets/admin/js/calendar/fullcalendar.min.js') }}"></script>
+<script src="{{ asset('assets/admin/js/calendar/fullcalendar-active.js') }}"></script>
+<!-- Include DataTables JS -->
+{{-- <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.11.5/js/jquery.dataTables.min.js"></script> --}}
 
 
-    <!-- plugins JS
+
+<!-- plugins JS
       ============================================ -->
-      <script src="{{ asset('assets/admin/js/plugins.js') }}"></script>
-  
+<script src="{{ asset('assets/admin/js/plugins.js') }}"></script>
+
+
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous">
+</script>
