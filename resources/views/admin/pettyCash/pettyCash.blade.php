592472243433ea7fba528ded2f77717c574d40c4
rajeshtiwari2503 <rajeshtiwari2503@gmail.com>
2024-02-19 14:17:32 +0530

    e

--- resources/views/admin/pettyCash/pettyCash.blade.php
+++ resources/views/admin/pettyCash/pettyCash.blade.php
@@ -82,7 +82,7 @@
                                         </tr>
                                     </thead>
                                     <tbody>
-                                    
+
                                             <tr>
                                                 <td>
                                                     1
@@ -106,12 +106,12 @@
                                                     </div>
                                                 </td>
                                             </tr>
-                                            @endforeach
+                                            {{-- @endforeach
                                         @else
                                         <tr>
                                             <td colspan="9" class="text-center">No Record Found</td>
                                         </tr>
-                                        @endif
+                                        @endif --}}
                                     </tbody>
                                 </table>
                             </div>
