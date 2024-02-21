d8386b1c7c027c9e2718ae55214bf4b2e5bb4f51
xaltam6 <xaltamgit@gmail.com>
2024-02-21 15:39:38 +0530

    re

--- resources/views/admin/pettyCash/pettyCash.blade.php
+++ resources/views/admin/pettyCash/pettyCash.blade.php
@@ -1,11 +1,9 @@
 <style>
     .bg {
-    .bg {
         background-color: #fff !important;
         color: black !important;
     }
 
-    .margin_top {
 
     .margin_top {
         margin-top: -100px !important;
@@ -25,7 +23,6 @@
         color: white;
     }
 
-    .pb {
 
     .pb {
         margin-bottom: -50px !important;
@@ -45,110 +42,97 @@
                                 <h4 class=" text-headings">Petty Cash</h4>
                             </div>
                             <div class="text-right">
-                                <div class="table-actions">
-                                    <a href="{{ route('pettyCash/create') }}"
-                                        class="btn bg_button text-white rounded f-14 p- mr-3 float-left mb-2 mb-lg-0 mb-md-0">
-                                        <i class="fa fa-plus"></i> Create
-                                    </a>
-                                    <button type="button" id="exportButton"
-                                    class="btn border rounded f-14 p-    mr-3 mb-2 mb-lg-0 mb-md-0 float-left"
-                                    style="border-color: #0F1316 !important;">
-                                    <i class="fa fa-file-export"></i> Export
-                                </button>
-                                </div>
+                                <form action="{{ route('export-excel-csv') }}" method="POST" enctype="multipart/form-data">
+                                    <div class="table-actions">
+                                        <a href="{{ route('pettyCash/create') }}"
+                                            class="btn bg_button text-white rounded f-14 p- mr-3 float-left mb-2 mb-lg-0 mb-md-0">
+                                            <i class="fa fa-plus"></i> Create
+                                        </a>
+                                        <button type="buttton" id="export"
+                                            class="btn border rounded f-14 p-    mr-3 mb-2 mb-lg-0 mb-md-0 float-left"
+                                            style="border-color: #0F1316 !important;">
+                                            <i class="fa fa-file-export"></i> Export
+                                        </button>
+                                    </div>
+                                    {{-- @csrf
+                                <div>
+                                    <input type ="file" name="file">
+                                    <button type="submit" class="btn btn-primary mt-4">Export</button>
+                                </div> --}}
+                                </form>
                             </div>
                         </div>
                         <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
                             {{-- <div class="s002"> --}}
-                                <form action="{{ route('search/filter') }}" method="GET" >
-                                    @csrf
-                                    <div class="d-flex">
-                                        <div class=" ">
-                                            <div class="form-group">
-                                                {{-- <label for="dateInput" class="text-black-50">Select Start Date:</label> --}}
+                            <form action="{{ route('search/filter') }}" method="GET">
+                                @csrf
+                                <div class="d-flex">
+                                    <div class=" ">
+                                        <div class="form-group">
+                                            {{-- <label for="dateInput" class="text-black-50">Select Start Date:</label> --}}
 
-                                                <input type="text" name="start_date" id="start_date"
-                                                    placeholder="Select Start Date"
-                                                    class="form-control bg-white rounded text-black-50"
-                                                    style="width: 230px; height: 35px;" value="{{ old('start_date') }}">
-                                                @if ($errors->has('start_date'))
-                                                    <span class="text-danger">{{ $errors->first('start_date') }}</span>
-                                                @endif
-                                            </div>
-                                        </div>
-                                        <div class="container  d-flex gap-4 ">
-                                            <div class=" form-group">
-                                                {{-- <label for="dateInput" class="text-black-50">Select End Date:</label> --}}
-                                                <!-- Input with Bootstrap styling -->
-                                                <input type="text" id="end_date"
-                                                    class="form-control bg-white text-black-50 rounded" name="end_date"
-                                                    placeholder="Select End Date" style="width: 230px; height: 35px;"
-                                                    value="{{ old('end_date') }}">
-                                                @if ($errors->has('end_date'))
-                                                    <span class="text-danger">{{ $errors->first('end_date') }}</span>
-                                                @endif
-                                                    <span class="text-danger">{{ $errors->first('end_date') }}</span>
-                                                @endif
-                                            </div>
-                                            <div class="form-group" style="margin-top: ;">
-                                                <button class="btn-search btn bg_button text-white bg-gray-100 "
-                                                    type="submit">Search </button>
-                                            </div>
-                                        </div>
-                                        <div class=" ">
-                                            <div class="form-group">
-                                                {{-- <label for="dateInput" class="text-black-50">Select Start Date:</label> --}}
-                                                <input type="file" name="file" id="file" placeholder=""
-                                                    class="form-control bg-white rounded text-black-50"
-                                                    style="width: 230px; height: 35px;" value="">
-                                            </div>
+                                            <input type="text" name="start_date" id="start_date"
+                                                placeholder="Select Start Date"
+                                                class="form-control bg-white rounded text-black-50"
+                                                style="width: 230px; height: 35px;" value="{{ old('start_date') }}">
+                                            @if ($errors->has('start_date'))
+                                                <span class="text-danger">{{ $errors->first('start_date') }}</span>
+                                            @endif
                                         </div>
                                     </div>
-                                </form>
-                            </div>
-                        </div>
-                        <div class="container">
-                            <div class="pb">
-                                <h4 class=" text-headings">Petty Cash</h4>
-                            </div>
-                            <div class="text-right">
-                            </div>
-                            <div class="text-right">
-                                <div class="table-actions">
+                                    <div class="container  d-flex gap-4 ">
+                                        <div class=" form-group">
+                                            {{-- <label for="dateInput" class="text-black-50">Select End Date:</label> --}}
+                                            <!-- Input with Bootstrap styling -->
+                                            <input type="text" id="end_date"
+                                                class="form-control bg-white text-black-50 rounded" name="end_date"
+                                                placeholder="Select End Date" style="width: 230px; height: 35px;"
+                                                value="{{ old('end_date') }}">
+                                            @if ($errors->has('end_date'))
+                                                <span class="text-danger">{{ $errors->first('end_date') }}</span>
+                                            @endif
 
-                                    <a href="{{ route('pettyCash/create') }}"
-                                        class="btn bg_button text-white rounded f-14 p-2 mr-3 float-left mb-2 mb-lg-0 mb-md-0">
-                                        <i class="fa fa-plus"></i> Create
-                                    </a>
-                                    <a href="#"
-                                        class="btn dt-buttons rounded f-14 p-2 mr-3 mb-2 mb-lg-0 mb-md-0 float-left">
-                                        <i class="fa fa-file-export"></i> Export
-                                    </a>
-                                </div>
-                            </div>
-                            <form action="{{ route('export-excel-csv') }}" method="POST" enctype="multipart/form-data">
-                                @csrf
-                                <div>
-                                    <input type ="file" name="file">
-                                    <button type="submit" class="btn btn-primary mt-4">Export</button>
+                                        </div>
+                                        <div class="form-group" style="margin-top: ;">
+                                            <button class="btn-search btn bg_button text-white bg-gray-100 "
+                                                type="submit">Search </button>
+                                        </div>
+                                    </div>
+                                   
                                 </div>
                             </form>
                         </div>
-                        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-                            @if (Session::has('success'))
-                                <div class="alert alert-success">
-                                    {{ Session::get('success') }}
-                                </div>
-                            @endif
-                            @if (Session::has('error'))
-                                <div class="alert alert-danger">
-                                    {{ Session::get('error') }}
-                                </div>
-                            @endif
-                        </div>
-                        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 mt-4">
-                            <div class="">
-                                <table id="dataTable">
+                    </div>
+                    <div class="container">
+
+                        <form action="{{ route('export-excel-csv') }}" method="POST" enctype="multipart/form-data">
+                            @csrf
+                            <div class="d-flex gap-2">
+                                <input style="width: 230px; height: 35px;" type ="file" name="file" class="form-control bg-white rounded text-black-50">
+                                
+                                <button type="submit" style="border-color: #0F1316 !important;"
+                                    class="btn border rounded f-14 p-    mr-3 mb-2 mb-lg-0 mb-md-0 float-left">Import</button>
+                                
+                                
+                                
+                            </div>
+                        </form>
+                    </div>
+                    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                        @if (Session::has('success'))
+                            <div class="alert alert-success">
+                                {{ Session::get('success') }}
+                            </div>
+                        @endif
+                        @if (Session::has('error'))
+                            <div class="alert alert-danger">
+                                {{ Session::get('error') }}
+                            </div>
+                        @endif
+                    </div>
+                    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 mt-4">
+                        <div class="">
+                            <table id="dataTable">
                                 <table id="dataTable">
                                     <thead>
                                         <tr role="row">
@@ -195,78 +179,44 @@
                                                     </td>
                                                 </tr>
                                             @endforeach
-                                        @if (count($expenses) > 0)
-                                            @foreach ($expenses as $index => $item)
-                                                <tr>
-                                                    <td>{{ $index + 1 }}</td>
-                                                    <td>{{ $item->project_id }}</td>
-                                                    <td>{{ \Carbon\Carbon::parse($item->date)->format('d/m/Y') }}</td>
-                                                    <td>{{ $item->project->project_type }}</td>
-                                                    <td>{{ $item->project->project_name }}</td>
-                                                    {{-- <td>Abid</td> --}}
-                                                    <td>{{ $item->grandtotal }}</td>
-                                                    <td class="text-right">
-                                                        <div class="dropdown dropdown-action">
-                                                            <a href="#" class="action-icon" data-toggle="dropdown"
-                                                                aria-expanded="false"><img
-                                                                    src="{{ asset('assets/admin/img/icon/action.png') }}"
-                                                                    alt=""></a>
-                                                            <div class="dropdown-menu dropdown-menu-right">
-                                                                <a class="dropdown-item"
-                                                                    href="{{ route('expenses/view', $item->id) }}"><i
-                                                                        class="fa fa-eye m-r-5"></i> View</a>
-                                                                <a class="dropdown-item"
-                                                                    href="{{ route('expenses/edit', $item->id) }}"><i
-                                                                        class="fa fa-pencil m-r-5"></i> Edit</a>
-                                                                <a class="dropdown-item"
-                                                                    href="{{ route('expenses/delete', $item->id) }}"><i
-                                                                        class="fa fa-trash-o m-r-5"></i> Delete</a>
-                                                            </div>
-                                                        </div>
-                                                    </td>
-                                                </tr>
-                                            @endforeach
                                         @else
-                                            <tr>
-                                                <td colspan="7" class="text-center">No Record Found</td>
-                                            </tr>
                                             <tr>
                                                 <td colspan="7" class="text-center">No Record Found</td>
                                             </tr>
                                         @endif
                                     </tbody>
                                 </table>
-                            </div>
                         </div>
                     </div>
                 </div>
             </div>
-
-            <!-- Static Table Start -->
-
-            <!-- Static Table End -->
         </div>
+
+        <!-- Static Table Start -->
+
+        <!-- Static Table End -->
+    </div>
     </div>
 
     <!-- metisMenu JS
-                                            ============================================ -->
-                                            ============================================ -->
+                                                        ============================================ -->
+    ============================================ -->
     <script src="{{ asset('assets/admin/js/metisMenu/metisMenu.min.js') }}"></script>
     <script src="{{ asset('assets/admin/js/metisMenu/metisMenu-active.js') }}"></script>
     <!-- float JS
-                                                ============================================ -->
-                                                ============================================ -->
+                                                            ============================================ -->
+    ============================================ -->
     <script src="{{ asset('assets/admin/js/flot/jquery.flot.js') }}"></script>
     <script src="{{ asset('assets/admin/js/flot/jquery.flot.resize.js') }}"></script>
     <script src="{{ asset('assets/admin/js/flot/curvedLines.js') }}"></script>
     <script src="{{ asset('assets/admin/js/flot/flot-active.js') }}"></script>
     <!-- plugins JS
-                                                ============================================ -->
-                                                ============================================ -->
+                                                            ============================================ -->
+    ============================================ -->
     <script src="{{ asset('assets/admin/js/plugins.js') }}"></script>
     <!-- main JS
-                                            ============================================ -->
-                                            ============================================ -->
+                                                        ============================================ -->
+    ============================================ -->
     <script src="{{ asset('assets/admin/js/main.js') }}"></script>
 
     {{-- Data Table js code --}}
